4e605d8e02c455621d7768e3361709a1484ddf16
Nico Elayda <nico@elayda.com>
2019-03-31 10:53:54 +0900

    nx-loader: Update to v2.1.

--- nxsd/components/homebrew.py
+++ nxsd/components/homebrew.py
@@ -4,7 +4,8 @@ from nxsd.components import NXSDComponent
 from nxsd.config import settings
 from pathlib import Path
 
-HBLOADER_VERSION = 'v2.0.1'
+HBLOADER_VERSION = 'v2.1'
+HBLOADER_COMMIT_OR_TAG = '3af8c89'
 HBMENU_VERSION = 'v3.0.1'
 
 
@@ -68,7 +69,7 @@ class HomebrewComponent(NXSDComponent):
         with util.change_dir(component_root):
             build_commands = [
                 'git fetch origin',
-                'git checkout {version}'.format(version=HBLOADER_VERSION),
+                'git checkout {version}'.format(version=HBLOADER_COMMIT_OR_TAG),
                 'make',
             ]
             util.execute_shell_commands(build_commands)
